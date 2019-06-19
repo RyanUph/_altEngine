24c73b4d1b836b254c75ec1b66fa7a5754bfbfcc
RyanUph <45742819+RyanUph@users.noreply.github.com>
2019-06-19 15:58:06 -0600

    Testing localtime_s fix again

--- AltEngine/src/localtime.hpp
+++ AltEngine/src/localtime.hpp
@@ -5,3 +5,7 @@
 #ifndef localtime_s
 #define localtime_r(now, tstruct) (localtime_s(tstruct, now))
 #endif
+
+#ifndef localtime_r
+#define localtime_s(tstruct, now) (localtime_r(now, tstruct))
+#endif
