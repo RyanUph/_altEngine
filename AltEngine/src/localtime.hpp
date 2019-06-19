05b8774694f88ced365269049154aea3fb075795
Blue Slime <blueslimexdfodeu@gmail.com>
2019-06-19 17:58:32 -0400

    Trying to fix security issue

--- AltEngine/src/localtime.hpp
+++ AltEngine/src/localtime.hpp
@@ -1,7 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <time.h>
 #include <ctime>
 
-#pragma warning(disable : 4996)
 #ifndef localtime_s
-#define localtime_r(now, tstruct) (localtime_s(tstruct, now))
+#define localtime_s(tstruct, now) (localtime_r(now, tstruct))
 #endif
