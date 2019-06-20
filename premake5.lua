ad429f70221dafd708d0de9f36a2d5626160df41
RyanUph <45742819+RyanUph@users.noreply.github.com>
2019-06-19 19:00:22 -0600

    Updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,20 +1,25 @@
 workspace "AltEngine"
-   configurations { "Debug", "Release" }
+	architecture "x86"
+	architecture "x64"
+	configurations { "Debug", "Release" }
 
-   outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 project "AltEngine"
-   kind "ConsoleApp"
-   language "C++"
-   targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-   objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	location "AltEngine"
+	kind "ConsoleApp"
+	language "C++"
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-   files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.hpp", "%{prj.name}/src/**.cpp" }
-
-   filter "configurations:Debug"
-      defines { "DEBUG" }
-      symbols "on"
-
-   filter "configurations:Release"
-      defines { "NDEBUG" }
-      optimize "on"
+	systemversion "latest"
+	
+	files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.hpp", "%{prj.name}/src/**.cpp" }
+	
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "on"
+	
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "on"
